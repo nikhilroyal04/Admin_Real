416e7acafd240e45b5a25dacab89e538966456ee
saritapal0 <159772730+saritapal0@users.noreply.github.com>
2024-10-07 11:56:12 +0530

    update

--- src/components/Properties/PropertyView.jsx
+++ src/components/Properties/PropertyView.jsx
@@ -1,126 +1,93 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { EditPropertyData,fetchAllPropertyData } from '../../app/Slices/propertiesSlice';
 import {
   Box,
+  Button,
+  Flex,
   FormControl,
   FormLabel,
   Input,
-  Stack,
-  Heading,
-  Button,
+  Spinner,
+  Alert,
 } from '@chakra-ui/react';
 
-function PropertyView() {
-  const [property, setProperty] = useState({
-    id: '12345', // Example pre-filled ID
-    propertyFor: '',
-    type: '',
-    subtype: '',
-    size: '',
-    location: '',
-    subLocation: '',
-  });
+const PropertyView = ({ propertyId }) => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const property = useSelector((state) => state.properties[propertyId]);
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setProperty({
-      ...property,
-      [name]: value,
-    });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchAllPropertyData(propertyId)); // Assume this action fetches property data
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [dispatch, propertyId]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    const formData = {
+      name: event.target.name.value,
+      type: event.target.type.value,
+      subtype: event.target.subtype.value,
+      size: event.target.size.value,
+      location: event.target.location.value,
+      subLocation: event.target.subLocation.value,
+    };
+
+    dispatch(EditPropertyData(propertyId, formData)); // Dispatch the edit action
   };
 
-  const handleSave = () => {
-    // Logic to save the property details
-    console.log('Saved property:', property);
-    // You can add your save logic here (e.g., API call)
-  };
+  if (loading) {
+    return <Spinner size="xl" />;
+  }
+
+  if (error) {
+    return <Alert status="error">{error}</Alert>;
+  }
 
   return (
-    <Box p={5}>
-      <Heading mb={4}>Edit Property Details</Heading>
-      <Stack spacing={4}>
-        <FormControl>
-          <FormLabel htmlFor="id">ID:</FormLabel>
-          <Input
-            type="text"
-            id="id"
-            name="id"
-            placeholder="Enter ID"
-            value={property.id}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="propertyFor">Property For:</FormLabel>
-          <Input
-            type="text"
-            id="propertyFor"
-            name="propertyFor"
-            placeholder="Enter Property For"
-            value={property.propertyFor}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="type">Type:</FormLabel>
-          <Input
-            type="text"
-            id="type"
-            name="type"
-            placeholder="Enter Type"
-            value={property.type}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="subtype">Subtype:</FormLabel>
-          <Input
-            type="text"
-            id="subtype"
-            name="subtype"
-            placeholder="Enter Subtype"
-            value={property.subtype}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="size">Size:</FormLabel>
-          <Input
-            type="text"
-            id="size"
-            name="size"
-            placeholder="Enter Size"
-            value={property.size}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="location">Location:</FormLabel>
-          <Input
-            type="text"
-            id="location"
-            name="location"
-            placeholder="Enter Location"
-            value={property.location}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="subLocation">SubLocation:</FormLabel>
-          <Input
-            type="text"
-            id="subLocation"
-            name="subLocation"
-            placeholder="Enter SubLocation"
-            value={property.subLocation}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <Button colorScheme="blue" onClick={handleSave}>
-          Save
-        </Button>
-      </Stack>
-    </Box>
+    <form onSubmit={handleSubmit}>
+      <Flex direction="column" mb={4}>
+        <Flex wrap="wrap" gap={4}>
+          <FormControl>
+            <FormLabel htmlFor="name">Property For</FormLabel>
+            <Input id="name" name="name" defaultValue={property?.name} required />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="type">Type</FormLabel>
+            <Input id="type" name="type" defaultValue={property?.type} required />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="subtype">Subtype</FormLabel>
+            <Input id="subtype" name="subtype" defaultValue={property?.subtype} required />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="size">Size</FormLabel>
+            <Input id="size" name="size" defaultValue={property?.size} required />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="location">Location</FormLabel>
+            <Input id="location" name="location" defaultValue={property?.location} required />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="subLocation">SubLocation</FormLabel>
+            <Input id="subLocation" name="subLocation" defaultValue={property?.subLocation} required />
+          </FormControl>
+        </Flex>
+      </Flex>
+      <Button type="submit" colorScheme="blue">Update Property</Button>
+    </form>
   );
-}
+};
 
 export default PropertyView;
